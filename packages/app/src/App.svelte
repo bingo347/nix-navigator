ab917110f3ba9505950e656d7fb7e4611152351a
Dmitry Belyaev <mail@d-belyaev.ru>
2026-07-12 20:34:54 +0300

    ui: add top toolbar

--- packages/app/src/App.svelte
+++ packages/app/src/App.svelte
@@ -1,18 +1,5 @@
 <script lang="ts">
-import { RadioButtonGroup, RadioButton } from "carbon-components-svelte";
-import {Theme, theme} from "./utils/theme";
-
+import TopToolbar from "./TopToolbar.svelte";
 </script>
 
-<h1>Nix Navigator</h1>
-
-<RadioButtonGroup
-    orientation="vertical"
-    legendText="Theme"
-    name="theme"
-    bind:selected={$theme}
->
-    <RadioButton labelText="System" value="{Theme.System}" />
-    <RadioButton labelText="Dark" value="{Theme.Dark}" />
-    <RadioButton labelText="Light" value="{Theme.Light}" />
-</RadioButtonGroup>
+<TopToolbar />
---
+++ packages/app/src/TopToolbar.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { Button, Toolbar, ToolbarContent, ContentSwitcher, Switch } from "carbon-components-svelte";
+import { View, ViewFilled, IntrusionPrevention, Settings } from "carbon-icons-svelte";
+import { theme } from "./utils/theme";
+
+const margin = 5;
+const style = `margin-left:${margin}px;margin-right:${margin}px;`;
+</script>
+
+<Toolbar size="sm">
+    <ToolbarContent>
+        <ContentSwitcher lowContrast size="sm" bind:selectedIndex={$theme} {style}>
+            <Switch
+                icon={IntrusionPrevention}
+                text="Theme: System"
+                tooltipAlignment="end"
+            />
+            <Switch
+                icon={View}
+                text="Theme: Light"
+                tooltipAlignment="end"
+            />
+            <Switch
+                icon={ViewFilled}
+                text="Theme: Dark"
+                tooltipAlignment="end"
+            />
+        </ContentSwitcher>
+        <Button
+            kind="tertiary"
+            size="small"
+            icon={Settings}
+            tooltipAlignment="end"
+            iconDescription="Configuration"
+            class="toolbar-element"
+            {style}
+        />
+    </ToolbarContent>
+</Toolbar>
